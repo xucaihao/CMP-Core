078e959a50da26c542a01778cf79459f4c32eee3
xucaihao <467046520@qq.com>
2018-04-09 16:23:47 +0800

    更新数据库脚本

--- src/sql/core.sql
+++ src/sql/core.sql
@@ -12,25 +12,27 @@ File Encoding         : 65001
 
 Date: 2018-04-07 18:42:28
 */
+BEGIN;
 
+CREATE Database IF NOT EXISTS cloudmp DEFAULT utf8 COLLATE utf8_bin;
+USER cloudmp;
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for tbl_cloud
 -- ----------------------------
-DROP TABLE IF EXISTS `tbl_cloud`;
-CREATE TABLE `tbl_cloud` (
-  `cloud_id` varchar(36) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '云ID',
-  `cloud_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '云名称',
-  `cloud_type` varchar(20) CHARACTER SET utf8 NOT NULL COMMENT '云类型值',
-  `visibility` varchar(36) CHARACTER SET utf8 NOT NULL,
-  `cloud_protocol` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `cloud_ip` varchar(36) CHARACTER SET utf8 NOT NULL,
-  `cloud_port` varchar(10) CHARACTER SET utf8 NOT NULL,
-  `status` varchar(36) CHARACTER SET utf8 NOT NULL,
-  `description` varchar(100) CHARACTER SET utf8 NOT NULL,
+CREATE TABLE IF NOT EXISTS `tbl_cloud` (
+  `cloud_id` varchar(36) NOT NULL DEFAULT COMMENT '云ID',
+  `cloud_name` varchar(50) NOT NULL COMMENT '云名称',
+  `cloud_type` varchar(20) NOT NULL COMMENT '云类型值',
+  `visibility` varchar(36) NOT NULL COMMENT '可见类型',
+  `cloud_protocol` varchar(20) NOT NULL COMMENT '网络协议',
+  `cloud_ip` varchar(36) NOT NULL COMMENT 'ip地址',
+  `cloud_port` varchar(10) NOT NULL COMMENT '端口号',
+  `status` varchar(36) NOT NULL COMMENT '云状态',
+  `description` varchar(100) NOT NULL COMMENT '描述',
   PRIMARY KEY (`cloud_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of tbl_cloud
@@ -42,14 +44,13 @@ INSERT INTO `tbl_cloud` VALUES ('43634264-196e-47f4-a673-3cdb48d9b780', 'ali3', 
 -- ----------------------------
 -- Table structure for tbl_cloud_type
 -- ----------------------------
-DROP TABLE IF EXISTS `tbl_cloud_type`;
-CREATE TABLE `tbl_cloud_type` (
-  `id` varchar(36) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '云ID',
-  `type_name` varchar(20) CHARACTER SET utf8 NOT NULL COMMENT '云类型值',
-  `type_value` varchar(36) CHARACTER SET utf8 NOT NULL,
-  `disable` varchar(36) CHARACTER SET utf8 NOT NULL,
+CREATE TABLE IF NOT EXISTS `tbl_cloud_type` (
+  `id` varchar(36) NOT NULL COMMENT '云类型ID',
+  `type_name` varchar(20) NOT NULL COMMENT '云类型名',
+  `type_value` varchar(36) NOT NULL COMMENT '云类型值',
+  `disable` varchar(36) NOT NULL COMMENT '启用状态',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of tbl_cloud_type
@@ -60,17 +61,16 @@ INSERT INTO `tbl_cloud_type` VALUES ('2', '腾讯云', 'tencent-cloud', '0');
 -- ----------------------------
 -- Table structure for tbl_user
 -- ----------------------------
-DROP TABLE IF EXISTS `tbl_user`;
-CREATE TABLE `tbl_user` (
-  `user_id` varchar(36) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '云ID',
-  `user_name` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '云名称',
-  `password` varchar(36) CHARACTER SET utf8 NOT NULL COMMENT '云类型值',
-  `role_name` varchar(36) CHARACTER SET utf8 NOT NULL,
-  `token` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `phone` varchar(36) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8 NOT NULL,
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `user_id` varchar(36) NOT NULL COMMENT '用户ID',
+  `user_name` varchar(50) NOT NULL COMMENT '用户登录名',
+  `password` varchar(36) NOT NULL COMMENT '密码',
+  `role_name` varchar(36) NOT NULL COMMENT '角色',
+  `token` varchar(100) NOT NULL COMMENT '密钥',
+  `phone` varchar(36) NOT NULL COMMENT '电话号码',
+  `email` varchar(100) NOT NULL COMMENT '邮箱',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of tbl_user
@@ -81,17 +81,16 @@ INSERT INTO `tbl_user` VALUES ('be59d9c8-cf9f-411c-a054-8df3b65152d4', 'xuhao', 
 -- ----------------------------
 -- Table structure for tbl_user_mapping
 -- ----------------------------
-DROP TABLE IF EXISTS `tbl_user_mapping`;
-CREATE TABLE `tbl_user_mapping` (
-  `id` varchar(36) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '云ID',
-  `cmp_user_id` varchar(36) CHARACTER SET utf8 NOT NULL COMMENT '云名称',
-  `cmp_user_name` varchar(36) CHARACTER SET utf8 NOT NULL COMMENT '云类型值',
-  `access_key` varchar(36) CHARACTER SET utf8 NOT NULL,
-  `auth_info` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `cloud_id` varchar(36) CHARACTER SET utf8 NOT NULL,
+CREATE TABLE IF NOT EXISTS `tbl_user_mapping` (
+  `id` varchar(36) NOT NULL COMMENT '用户映射id',
+  `cmp_user_id` varchar(36) NOT NULL COMMENT '用户id',
+  `cmp_user_name` varchar(36) NOT NULL COMMENT '用户登录名',
+  `access_key` varchar(36) NOT NULL COMMENT '云accessKey',
+  `auth_info` varchar(100) NOT NULL COMMENT '云上用户认证信息',
+  `cloud_id` varchar(36) NOT NULL COMMENT '云id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+s
 -- ----------------------------
 -- Records of tbl_user_mapping
 -- ----------------------------
