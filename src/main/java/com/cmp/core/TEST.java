3e1d927260a0eae05ce45c07293873a8d1e08fd3
xucaihao <467046520@qq.com>
2018-08-10 00:13:51 +0800

    增加硬盘列表界面

--- src/main/java/com/cmp/core/TEST.java
+++ src/main/java/com/cmp/core/TEST.java
@@ -4,8 +4,6 @@ public class TEST {
 
     public static void main(String[] args) {
         System.out.printf("old");
-    }
-    public static void main(String[] args) {
         System.out.printf("new");
     }
 }
