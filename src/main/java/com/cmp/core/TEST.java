76b1b2a2f00ae27d21366e1f89dca2eb5fb0e407
xucaihao <467046520@qq.com>
2018-08-10 00:17:27 +0800

    增加硬盘列表界面

--- src/main/java/com/cmp/core/TEST.java
+++ src/main/java/com/cmp/core/TEST.java
@@ -3,6 +3,8 @@ package com.cmp.core;
 public class TEST {
 
     public static void main(String[] args) {
-        System.out.printf("new");
+        System.out.print("new");
+        System.out.print("");
+        System.out.println();
     }
 }
